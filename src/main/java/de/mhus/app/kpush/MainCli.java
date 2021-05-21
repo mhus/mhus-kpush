0b5afae5a8ec9c0e8b8c13130f105d57f577cd31
Mike Hummel <mike@mhus.de>
2021-05-21 21:58:45 +0200

    Update MainCli.java

--- src/main/java/de/mhus/app/kpush/MainCli.java
+++ src/main/java/de/mhus/app/kpush/MainCli.java
@@ -11,15 +11,17 @@ public class MainCli {
         
         MArgs margs = new MArgs(args,
         		MArgs.help("Tool to watch and push changed files in a kubernetes pod"),
-        		MArgs.optVal("l", "Log level trace, debug, info, warn, error, fatal, default is INFO"),
-        		MArgs.optVal("t", "Time interval, e.g. 1d, 1h, 1min, default is now"),
-        		MArgs.optVal("i", "Interval in milliseconds for watch command, default is 5000"),
-        		MArgs.optVal("c", "Configuration directory or file, default is '~/.kpush/config'"),
+        		MArgs.optVal('l', "Log level trace, debug, info, warn, error, fatal, default is INFO"),
+        		MArgs.optVal('t', "Time interval, e.g. 1d, 1h, 1min, default is now"),
+        		MArgs.optVal('i', "Interval in milliseconds for watch command, default is 5000"),
+        		MArgs.optVal('c', "Configuration directory or file, default is '~/.kpush/config'"),
         		MArgs.arg("cmd", "Command:\ntest - Test push\ntouch - Touch sync to time interval\npush - Push files changed since last push,\npushall - Push all files\nwatch - Watch all files in a loop"),
         		MArgs.argAll("filter", "Filter configurations")
         		);
-        if (!margs.isValid())
-        	System.exit(1);
+        if (!margs.isPrintUsage()) {
+        	margs.printUsage();
+        	System.exit(margs.isValid() ? 0 : 1);
+        }
         
         String verbose = margs.getOption("v").getValue();
         if (verbose != null) {
